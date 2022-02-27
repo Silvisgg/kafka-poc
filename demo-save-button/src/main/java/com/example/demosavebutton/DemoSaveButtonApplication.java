f2be79f8cb453ef05df9c036c3766631665b755f
silvis_sil <silvis.garcia@gmail.com>
2022-02-27 20:19:46 +0100

    Init commit

---
+++ demo-save-button/src/main/java/com/example/demosavebutton/DemoSaveButtonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosavebutton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSaveButtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSaveButtonApplication.class, args);
+	}
+
+}
---
+++ demo-save-button/src/test/java/com/example/demosavebutton/DemoSaveButtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosavebutton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSaveButtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
